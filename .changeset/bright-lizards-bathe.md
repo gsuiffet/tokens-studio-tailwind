cd42c2e81e86987bec3bd18b6a3d2e2760bec351
Guillaume <suiffet.g@gmail.com>
2023-12-15 00:06:22 +0100

    refactor border color

---
+++ .changeset/bright-lizards-bathe.md
@@ -0,0 +1,5 @@
+---
+'@gsuiffet/tokens-studio-tailwind': patch
+---
+
+refactor border
--- src/CSSComponents/border.ts
+++ src/CSSComponents/border.ts
@@ -1,4 +1,4 @@
-import { DesignToken, TransformedToken } from 'style-dictionary';
+import { TransformedToken } from 'style-dictionary';
 import { SingleBorderToken, TokenBorderValue, TokenTypes } from '@tokens-studio/types';
 import {
   ALLOWED_BORDER_STYLES,
@@ -8,7 +8,6 @@ import {
 } from '../types';
 import { getSizeValue } from '../CSSVariables';
 import { VALUE_NOT_ALLOWED } from '../utils';
-import { get, kebabCase } from 'lodash';
 
 const { SOLID } = TokenBorderStyleValue;
 const { TW_CLS_BORDER } = TailwindClassTypes;
@@ -16,29 +15,15 @@ const { BORDER_WIDTH } = TokenTypes;
 
 type Props = TransformedToken | SingleBorderToken;
 
-function getColor(color: string | undefined, original: DesignToken) {
-  const isVariable = new RegExp('^{.*}$');
-  if (color) {
-    if (get(original, 'value.color') && isVariable.test(original.value.color)) {
-      return `sd-${kebabCase(original.value.color.replace(/^\{|}$/g, ''))}`;
-    } else {
-      return `[${color}]`;
-    }
-  } else {
-    return `[${TW_COLOR_DEFAULT_VALUE}]`;
-  }
-}
-
 export function getBorderValue(token: Props): string[] {
   const { value, type } = token;
-  const { original } = token as TransformedToken;
   const { color, width, style } = value as TokenBorderValue;
   if (style && !ALLOWED_BORDER_STYLES.includes(style as TokenBorderStyleValue)) {
     throw new Error(VALUE_NOT_ALLOWED(style, type, ALLOWED_BORDER_STYLES));
   }
 
   return [
-    `${TW_CLS_BORDER}${getColor(color, original)}`,
+    `${TW_CLS_BORDER}[${color?.replace(/\s/g, '') ?? TW_COLOR_DEFAULT_VALUE}]`,
     `${TW_CLS_BORDER}[${getSizeValue({
       value: width ?? '',
       type: BORDER_WIDTH,
